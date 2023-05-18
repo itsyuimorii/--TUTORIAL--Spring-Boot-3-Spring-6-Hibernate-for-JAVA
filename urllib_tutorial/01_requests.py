be81e5a7d01a8f7c8ce7c72a988199e5de5afcbc
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-18 14:18:07 -0500

    Update 01_requests.py

--- urllib_tutorial/01_requests.py
+++ urllib_tutorial/01_requests.py
@@ -8,14 +8,17 @@ import requests as requests
 
 
 # (1) Define an url that is the address you want to access
-url = 'http://www.google.com'
+url = 'https://www.datacamp.com/cheat-sheet/sql-basics-cheat-sheet'
 
 # (2) simulate a browser sending a request to the server
 response = requests.get(url=url)
 
 # (3) Get the source code of the page in the response content meaning of the content
+print(type(response))
+# <class 'requests.models.Response'>
+
+print(response.text)
 
 
-# (4) Print the data
 
 
