2e690db53f8fef1033c9c902a65e761b78b15eed
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-18 15:14:40 -0500

    update

--- requests_tutorial/02_request_get.py
+++ requests_tutorial/02_request_get.py
@@ -2,3 +2,9 @@
 # @Author： Itsyuimoriispace
 # @File: request_get
 # @Project: Web Scraping in Python BeautifulSoup, Selenium & Scrapy 2023
+
+import requests
+
+x = requests.get('https://w3schools.com/python/demopage.htm')
+
+print(x.text)
