6bb84f063ed1b35ef7e0ae93944ce656602092d0
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-18 14:14:33 -0500

    clean urllib

--- urllib_tutorial/01_requests.py
+++ urllib_tutorial/01_requests.py
@@ -3,23 +3,19 @@
 # @Author： Itsyuimoriispace
 # @File: 01_urllib_basic_usage
 # @Project: Web Scraping in Python BeautifulSoup, Selenium & Scrapy 2023
-
+import requests as requests
 # Use urllib to get the source code for the Baidu homepage
-import urllib.request
 
 
 # (1) Define an url that is the address you want to access
 url = 'http://www.google.com'
 
 # (2) simulate a browser sending a request to the server
-response = urllib.request.urlopen(url)
+response = requests.get(url=url)
 
 # (3) Get the source code of the page in the response content meaning of the content
-# The read method returns binary data in byte form
-# We want to convert the binary data to a string
-# binary - "string decode ('format of encoding')
-content = response.read().decode('utf-8')
+
 
 # (4) Print the data
-print(content)
+
 
