6081177dfe7526980d7eb1472ee305d4918d884d
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-19 11:00:29 -0500

    Update 07_urllib_post.py

--- requests_tutorial/07_urllib_post.py
+++ requests_tutorial/07_urllib_post.py
@@ -9,3 +9,27 @@ headers = {
 'User-Agent':
 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 }
+
+data = {
+    'q': 'spider'
+}
+
+# post リクエストパラメータはエンコードする必要があります
+data = urllib.parse.urlencode(data).encode('utf-8')
+
+# post request parameters need to be placed in the custom parameters of the request object, not appended after the url
+# post request parameters need to be encoded
+request = urllib.request.Request(url=url, data=data, headers=headers)
+
+# ブラウザがサーバーにリクエストを送信する様子をシミュレートします。
+response = urllib.request.urlopen(request)
+
+# Get the response data
+content = response.read().decode('utf-8')
+
+# String --> json object
+
+import json
+
+obj = json.loads(content)
+print(obj)
