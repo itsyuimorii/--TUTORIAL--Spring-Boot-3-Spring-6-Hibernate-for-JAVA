bbb499031b148ada1aff1cd93151a7871f2cdf2d
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-19 08:19:44 -0500

    Update 04_request_objects.py

--- requests_tutorial/04_request_objects.py
+++ requests_tutorial/04_request_objects.py
@@ -10,8 +10,12 @@ headers = {
 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
 }
 
-response = urllib.request.urlopen(url)
-
+# Because the dictionary cannot be stored in the urlopen method, headers cannot be passed in
+'''
+Create a Request object to send an HTTP request. url is the destination URL to be requested and headers is a dictionary containing the headers to be added to the request. Here the request headers are passed through the headers parameter.
+'''
+request = urllib.request.Request(url=url, headers=headers)
+response = urllib.request.urlopen(request)
 content = response.read()
 
 print(content)
