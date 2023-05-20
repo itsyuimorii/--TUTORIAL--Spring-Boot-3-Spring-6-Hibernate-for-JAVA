f16c0c9e7bc8da7498c3f421cc1bbe873d33e8de
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-20 10:33:12 -0500

    Update 01_bs4.py

--- bs4/01_bs4.py
+++ bs4/01_bs4.py
@@ -1,12 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
+import requests  # Importing the requests library to send HTTP requests
+from bs4 import BeautifulSoup  # Importing the BeautifulSoup library for HTML parsing
+
+website = 'https://subslikescript.com/movie/Titanic-120338'  # The URL of the website to fetch content from
+result = requests.get(website)  # Sending a GET request to fetch the website's response
+content = result.text  # Extracting the content from the response
+soup = BeautifulSoup(content, 'lxml')  # Parsing the HTML content using BeautifulSoup with the lxml parser
 
 
+# print(soup.prettify())
 
-website = 'https://subslikescript.com/movie/Titanic-120338'
-result = requests.get(website)
-content = result.text
 
-soup = BeautifulSoup(content, 'lxml')
+box = soup.find('article',class_='main-article')
+title = box.find('h1').get_text()
+transcript = box.find('div', class_='full-script').get_text(strip = True, separator=' ')
+print(title)
+print(transcript)
 
-print(soup.prettify())
