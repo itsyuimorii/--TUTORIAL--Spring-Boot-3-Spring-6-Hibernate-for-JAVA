eabde674db1a525070d3e4da8dc5fd8388ecc641
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-21 16:41:52 -0500

    Create 02_selenium.py

---
+++ selenium/02_selenium.py
@@ -0,0 +1,17 @@
+# driver.find_element_by_id('id')
+# driver_find_element_by_class_name('class_name')
+
+'''
+driver.find _element_by_class _name('main-article')
+driver.find_element_by_class _name('plot')
+driver. find_element_by_class _name('full-script')
+driver.find_element_by_id('id')
+driver_find_element_by_class_name('class_name')
+
+driver.find_element_by_xpath(//tag[@AttributeName=
+"Value"]')
+'''
+
+
+from selenium import webdriver
+
