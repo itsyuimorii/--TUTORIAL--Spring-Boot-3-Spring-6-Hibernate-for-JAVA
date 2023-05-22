d4956b9944e1ca78f61ec014fd6c40f72162c61b
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-22 08:00:39 -0500

    clear commit history

---
+++ urllib_tutorial/03_urllib_download.py
@@ -0,0 +1,14 @@
+
+import urllib.request
+
+# 1. download page
+# url_page = 'http://www.google.com'
+# parameter1: url, parameter2: filename
+# urllib.request.urlretrieve(url_page, "filename.html")
+
+# 2. download image
+url_image = ''
+urllib.request.urlretrieve(url_image, "imagename.jpg")
+
+url = 'http://www.google.com'
+
