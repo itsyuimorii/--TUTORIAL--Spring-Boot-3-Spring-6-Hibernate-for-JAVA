4f8959a4a96d783933f545c62305624cb0daa640
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-21 23:17:41 -0500

    selenium football_data

--- selenium/03_selenium.py
+++ selenium/03_selenium.py
@@ -5,6 +5,7 @@ import time
 
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
@@ -28,6 +29,12 @@ def main():
     all_matches_button = driver.find_element('xpath', '//label[@analytics-event="All matches"]')
     all_matches_button.click()
 
+
+    dropdown = Select(driver.find_element(By.ID, "country"))
+    dropdown.select_by_visible_text('Spain')
+    # implicit wait (useful in JavaScript driven websites when elements need seconds to load and avoid error "ElementNotVisibleException")
+    time.sleep(3)
+
     elements = driver.find_elements(By.TAG_NAME, 'tr')
 
     for element in elements:
@@ -46,7 +53,7 @@ def main():
 def export(date, home_team, score, away_team):
     # Create Dataframe in Pandas and export to CSV (Excel)
     df = pd.DataFrame({'date': date, 'home_team': home_team, 'score': score, 'away_team': away_team})
-    df.to_csv('football_data.csv', index=False)
+    df.to_csv('football_data_spain.csv', index=False)
     print(df)
 
 
