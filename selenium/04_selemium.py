8f7c5c96c0e0e9210a3d4ece27aab64ee0d21e2f
itsyuimorii.tech ♥ <109933604+itsyuimorii@users.noreply.github.com>
2023-05-22 00:18:37 -0500

    Update 04_selemium.py

--- selenium/04_selemium.py
+++ selenium/04_selemium.py
@@ -1,1 +1,17 @@
- 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+web = "https://www.audible.ca/search"
+service = Service(
+    '/Users/itsyuimoriispace/Documents/GitHub/Web Scraping in Python BeautifulSoup, Selenium & Scrapy 2023/chromedriver_mac64/chromedriver')
+# define 'driver' variable
+driver = webdriver.Chrome(service=service)
+driver.get(web)
+driver.maximize_window()
+
+
+container = driver.find_element(By.CLASS_NAME, "adbl-impression-container ")
+products = container.find_element('xpath', './li')
+
+for product in products:
